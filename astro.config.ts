d2bb42abeaae041c3bbf7461a87e15908f28fe41
Oliver Speir <oliverspeir9@gmail.com>
2024-12-18 19:32:50 -0700

    remove tailwind

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 
 export default defineConfig({
 	site: "http://www.example.com",
-	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		sitemap(),
-		robotsTxt(),
-	],
+	integrations: [sitemap(), robotsTxt()],
 });
